e065aff0631b0e038dad177d2e6a6a5f67a56053
hoin <apicad0307@gmail.com>
2022-10-17 00:01:47 +0900

    create gigs project ver0.1

---
+++ src/main/java/gigsproject/gigs/GigsApplication.java
@@ -0,0 +1,13 @@
+package gigsproject.gigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gigsproject/gigs/GigsApplicationTests.java
@@ -0,0 +1,13 @@
+package gigsproject.gigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
