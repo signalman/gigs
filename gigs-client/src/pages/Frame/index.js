c520649020be7f7e13311168a7df99651f87b0ea
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-10-17 20:10:11 +0900

    feat: add header page

--- gigs-client/src/pages/Frame/index.js
+++ gigs-client/src/pages/Frame/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import Header from '../Header';
+import { Box, styled } from '@mui/material';
 
+const HeaderSpace = styled(Box)((p) => ({
+  height: '200px',
+}));
+
+const Body = styled(Box)((p) => ({
+  width: '100%',
+}));
+
+/**
+ * 헤더의 위치를 잡아주고 바디의 페이지를 라우팅해주는 페이지
+ */
 const Frame = () => {
   return (
   <>
-    <Button variant='contained'>MUI TEST</Button>
+    <Header />
+    <HeaderSpace />
+    {/* Carousel */}
+    <Body>
+      바디
+    </Body>
   </>
   );
 }
--- gigs-client/src/pages/Header/index.js
+++ gigs-client/src/pages/Header/index.js
@@ -1,7 +1,48 @@
+import { Box, styled } from '@mui/material';
 import React from 'react';
 
+const Container = styled(Box)((p) => ({
+  position: 'fixed',
+  width: '100%',
+  height: '200px',
+  backgroundColor: "red",
+}));
+
+const HeaderBox = styled(Box)((p) => ({
+  margin: "0 auto",
+  width: '1200px',
+  height: '100%',
+  backgroundColor: 'blue',
+  display: 'flex',
+}));
+
+const SmallBox = styled(Box)((p) => ({
+  width: '250px',
+  height: '100%',
+  backgroundColor: 'yellow',
+}));
+
+const MenuBtnBox = styled(Box)((p) => ({
+  width: '700px',
+  height: '100%',
+  backgroundColor: 'green',
+}));
+
+/**
+ * 화면 상단 위치에 고정된 헤더
+ */
 const Header = () => {
-  return (<></>);
+  return (
+  <>
+    <Container>
+      <HeaderBox>
+        <SmallBox>로고</SmallBox>
+        <MenuBtnBox>메뉴 버튼들</MenuBtnBox>
+        <SmallBox>마이메뉴</SmallBox>
+      </HeaderBox>
+    </Container>
+  </>
+  );
 }
 
 export default Header;
