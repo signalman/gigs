2a08e7452cef4d9f82a96dee7d879cae8e7c5082
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-10-17 10:13:04 +0900

    Feat: install react-router-dom

--- gigs-client/src/index.js
+++ gigs-client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // StrictMode는 개발 버전에서만 적용되는 모드로, 여러 부분에서 검사를 해주는 모드
+  // https://ko.reactjs.org/docs/strict-mode.html
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
