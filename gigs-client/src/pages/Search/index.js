40c7c6398aa0c1dff64277c6d9c1585ba106fae4
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-10-21 16:45:59 +0900

    feat: 그리드 1차 구현

--- gigs-client/src/pages/Search/index.js
+++ gigs-client/src/pages/Search/index.js
@@ -1,23 +1,105 @@
-import { Box } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { API } from '../../utils/Constants';
+import { Box, Grid } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import { API, PATH, SYMBOL } from '../../utils/Constants';
 import axios from 'axios';
 import SearchConditionBox from '../../components/SearchConditionBox';
 
+const PAGE_SIZE = Math.ceil(window.innerHeight / 500) * 3;
+const DEV = false;
+
+const debounce = (callback, limit) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback.apply(this, args);
+    }, limit);
+  }
+};
+
 const Search = () => {
-  const [starCards, setStarCards] = useState([]);
+  const [target, setTarget] = useState(null);
+  const [conditions, setConditions] = useState({});
+  const [sort, setSort] = useState("dateDesc");
+  const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isFetching, setFetching] = useState(false);
+  const [hasNextPage, setNextPage] = useState(true);
+
+  const fetchDataForStar = useCallback( async (newConditions = {}, newSort = "") => {
+    newConditions = newConditions || conditions;
+    newSort = newSort || sort;
+
+    try {
+      const response = await axios.get(API.getStarCards(newConditions, newSort, PAGE_SIZE, 0));
+
+      const data = response.data;
+      setCards(data);
+      setPage(1);
+    } catch (e) {
+      console.log(e);
+      
+      setCards([1, 2, 3, 4, 5, 6]);
+      setPage(1);
+    }
+  }, [conditions, sort]);
+  // TODO: fetchDataForStage
 
   useEffect(() => {
-    // TODO: API 개발 시 open
-    // const getStarCards = async () => {
-    //   const response = await axios.get(API.getStarCards('', [], [], '', '', 'dateDesc'));
-
-    //   return response.data;
-    // }
-    
-    // const data = getStarCards();
-    // setStarCards(data);
+    if(DEV) {
+      const newCards = [];
+      for(let i=0; i<PAGE_SIZE; i++) newCards.push(i);
+      setCards(newCards);
+      return;
+    }
+    // check pathname
+    const pathname = window.location.pathname;
+    if(pathname.indexOf(PATH.searchStar) > -1) {
+      setTarget(SYMBOL.star);
+      fetchDataForStar();
+    }
   }, []);
+
+  // 무한 스크롤 이벤트 등록
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      if(window.innerHeight + scrollTop >= offsetHeight) {
+        setFetching(true);
+      }
+    }, 100);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  const fetchDataForStarPaging = useCallback(async () => {
+    try {
+      const response = await axios.get(API.getStarCards(conditions, sort, PAGE_SIZE, page));
+      const data = response.data;
+      setCards(data);
+      setPage(page+1);
+      setFetching(false);
+      // TODO: hasNext 처리
+    } catch (e) {
+      console.log(e);
+      setCards([...cards, 1, 2, 3, 4, 5, 6]);
+      setPage(page+1);
+      setFetching(false);
+    }
+  }, [cards, conditions, sort, page]);
+
+  // 무한 스크롤 시 데이터 가져오기
+  useEffect(() => {
+    console.log("scroll!")
+    if(isFetching && hasNextPage) {
+      if(target === SYMBOL.star) {
+        fetchDataForStarPaging();
+      }
+    } else if(!hasNextPage) setFetching(false);
+  }, [isFetching]);
 
   return (
     <>
@@ -27,18 +109,21 @@ const Search = () => {
         }}
       >
         <Box
-          sx={{
-            width: '100%',
-          }}
-        >
-          <SearchConditionBox setStarCards={setStarCards} />
+          sx={{ width: '100%', }}>
+          <SearchConditionBox fetchDataForStar={fetchDataForStar} setConditions={setConditions} setParentSort={setSort} />
         </Box>
         <Box
           sx={{
             width: '100%',
           }}
         >
-
+          <Grid container spacing={'75px'} sx={{ pl: '75px', }}>
+            {cards?.map((card, i) => (
+              <Grid item key={i}>
+                <Box sx={{width: '300px', height: '450px', backgroundColor: 'red',}}></Box>
+              </Grid>  
+            ))}
+          </Grid>
         </Box>
       </Box>
     </>
