ae9ced501f66bd454236a41768ea6807262045cd
FriedEggChicken <sonheungminson@gmail.com>
2022-11-07 19:52:37 +0900

    feat:회원가입페이지

--- gigs-client/src/pages/Frame/index.js
+++ gigs-client/src/pages/Frame/index.js
@@ -10,6 +10,7 @@ import {
 } from 'react-router-dom';
 import Search from '../Search';
 import { SYMBOL } from '../../utils/Constants';
+import SignUp from '../SignUp';
 
 /**
  * 헤더의 위치를 잡아주고 바디의 페이지를 라우팅해주는 페이지
@@ -27,6 +28,7 @@ const Frame = () => {
         <Route exact path="/stage" element={<Search target={SYMBOL.stage}></Search>} />
         <Route exact path="/star" element={<Search target={SYMBOL.star}></Search>} />
         <Route path="/review" element={<Main>이용 후기</Main>} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </Box>
   </>
---
+++ gigs-client/src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpForm from '../../components/SingUpForm';
+
+const SignUp = (
+
+) => {
+  return (
+    <>
+        <SignUpForm/>
+    </>
+  );
+};
+
+export default SignUp;
