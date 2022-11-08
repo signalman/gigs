6183c0f02a05bf4d57ddccdc03c3b8cc1f9513cb
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-11-08 20:48:52 +0900

    feat: 날짜에 추가 데이터 표시

--- gigs-client/src/pages/Main/index.js
+++ gigs-client/src/pages/Main/index.js
@@ -1,8 +1,11 @@
-import { Box, styled } from '@mui/material';
-import React from 'react';
-import StaticDatePicker from '@mui/x-date-pickers/StaticDatePicker';
+import { Box, TextField, styled } from '@mui/material';
+import React, { useState } from 'react';
+import {StaticDatePicker} from '@mui/x-date-pickers/StaticDatePicker';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay } from '@mui/x-date-pickers';
+import moment from 'moment';
+
 
 const Container = styled(Box)((p) => ({
   width: '1200px',
@@ -18,6 +21,8 @@ const Container = styled(Box)((p) => ({
 const Main = ({
   children,
 }) => {
+  const [selectedDay, setSelectedDay] = useState(moment());
+
   return (
     <>
       <Container>
@@ -33,7 +38,36 @@ const Main = ({
               width: '600px',
             }}
           >
-            
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              <StaticDatePicker
+                displayStaticWrapperAs="desktop"
+                value={selectedDay}
+                onChange={(e) => {
+                  setSelectedDay(e);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+                renderDay={(days, selectedDays, pickerDayProps) => {
+                  // console.log(days);
+                  console.log(selectedDays[0].isSame(days));
+                  console.log(selectedDays[0].month());
+
+                  console.log(pickerDayProps);
+
+                  // return (<PickersDay day={days} outsideCurrentMonth={days.month() !== selectedDays[0].month()} selected={selectedDays[0].isSame(days)} onDaySelect={() => {}} />)
+                  return (
+                    <Box sx={{ position: 'relative' }}>
+                      <PickersDay {...pickerDayProps} />
+                      {pickerDayProps.outsideCurrentMonth ? (<></>) : (<Box sx={{ position: 'absolute', top: 0, right: 0, color: 'red', fontSize: '9px' }}>
+                        15
+                      </Box>)}
+                      
+                    </Box>
+                  )
+                }}
+              >
+
+              </StaticDatePicker>
+            </LocalizationProvider>
           </Box>
         </Box>
       </Container>
