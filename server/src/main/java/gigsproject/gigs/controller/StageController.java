5f755a26c5d8be03a2bc4f85fde617379c4dcaea
win_min <60373714+psm9718@users.noreply.github.com>
2022-11-11 21:58:31 +0900

    feat: 무대 검색 및 무대 상세정보 조회 기능

--- server/src/main/java/gigsproject/gigs/controller/StageController.java
+++ server/src/main/java/gigsproject/gigs/controller/StageController.java
@@ -1,34 +1,42 @@
 package gigsproject.gigs.controller;
 
-import gigsproject.gigs.request.PostSave;
 import gigsproject.gigs.request.StageSearch;
+import gigsproject.gigs.response.HostResponse;
 import gigsproject.gigs.response.StageCard;
 import gigsproject.gigs.service.PostService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class StageController {
 
-    private final PostService postService;
-
+    private final PostService hostService;
+    /**
+     *  무대 검색
+     */
     @GetMapping("/stages")
     public Page<StageCard> getList(@ModelAttribute StageSearch stageSearch, @PageableDefault(size = 10) Pageable pageable) {
-        return postService.getList(stageSearch, pageable);
+        return hostService.getList(stageSearch, pageable);
     }
 
-    @PostMapping("/posts")
-    public void PostSave(@RequestBody @Valid PostSave postSave){
-        postService.write(postSave);
+    /**
+     * 무대 상세 정보 조회
+     */
+    @GetMapping("/stages/{hostId}")
+    public HostResponse stageInfo(@PathVariable Long hostId){
+        return hostService.findHost(hostId);
     }
+
 
 
 }
--- server/src/main/java/gigsproject/gigs/response/HostResponse.java
+++ server/src/main/java/gigsproject/gigs/response/HostResponse.java
@@ -1,24 +1,22 @@
 package gigsproject.gigs.response;
 
 import gigsproject.gigs.domain.*;
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
 
 @Getter
-public class StageCard {
+public class HostResponse {
 
-    private Long hostId;
-    private String imgUrl;
+    private final Long hostId;
+    private List<StageImg> imgUrl;
     private String stageName;
-    private Address address;
+    private final Address address;
+    private StageType stageType;
+    private String stageInfo;
 
     private Double stageSize;
     private String targetGender;
@@ -30,18 +28,21 @@ public class StageCard {
     private Integer targetMinCount;
 
     private Integer pay;
-    private StageType stageType;
-//    private List<PostGenreDto> genres;
-
     private Double avgScore;
     private Integer showCount;
     private Integer reviewCount;
 
-    public StageCard(Host host) {
+
+    private List<Post> posts;
+    private List<PostGenreDto> genres;
+
+
+    public HostResponse(Host host) {
         this.hostId = host.getHostId();
-        this.imgUrl = host.getImgs().isEmpty() ? "empty" : host.getImgs().get(0).getUrl();
-        this.stageSize = host.getStageSize();
         this.stageName = host.getStageName();
+        this.stageInfo = host.getStageInfo();
+        this.imgUrl = host.getImgs().isEmpty() ? List.of() : host.getImgs();
+        this.stageSize = host.getStageSize();
         this.address = host.getStageAddress();
         this.targetGender = String.valueOf(host.getTargetGender());
         this.targetAge = host.getTargetAge();
@@ -53,14 +54,8 @@ public class StageCard {
         this.pay = host.getPay();
         this.stageType = host.getStageType();
 
-//        this.genres = host.getPosts().
-//        this.genres = post.getPostGenres().stream()
-//                .map(postGenre -> new PostGenreDto(postGenre))
-//                .collect(Collectors.toList());
-
         this.showCount = isNull(host.getShowCount()) ? 0 : host.getShowCount();
         this.reviewCount = isNull(host.getReviewCount()) ? 0 : host.getReviewCount();
         this.avgScore = isNull(host.getAvgScore()) ? 0 : host.getAvgScore();
-
     }
 }
