691eec82aef6185c343f043ed134ebd58cf8602b
hoin <apicad0307@gmail.com>
2022-11-15 21:02:10 +0900

    feat: 공연기록 History Dto 생성

--- server/src/main/java/gigsproject/gigs/response/History.java
+++ server/src/main/java/gigsproject/gigs/response/History.java
@@ -1,2 +1,23 @@
-package gigsproject.gigs.response;public class History {
+package gigsproject.gigs.response;
+
+import gigsproject.gigs.domain.Proposal;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class History {
+    private Long hostId;
+    private String stageName;
+    private Long starId;
+    private String starName;
+    private LocalDateTime dateTime;
+
+    History(Proposal proposal) {
+        this.hostId = proposal.getHost().getHostId();
+        this.stageName = proposal.getHost().getStageName();
+        this.starId = proposal.getStar().getStarId();
+        this.starName = proposal.getStar().getName();
+        this.dateTime = proposal.getShowStartTime();
+    }
 }
