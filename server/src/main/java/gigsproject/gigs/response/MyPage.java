f7c76b595f23eefc28893f0c8c805a11525d8298
hoin <apicad0307@gmail.com>
2022-11-15 21:02:40 +0900

    feat: MyPage View를 담당하는 MyPage Dto 생성

--- server/src/main/java/gigsproject/gigs/response/MyPage.java
+++ server/src/main/java/gigsproject/gigs/response/MyPage.java
@@ -1,2 +1,19 @@
-package gigsproject.gigs.response;public class MyPage {
+package gigsproject.gigs.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MyPage {
+
+    //유저 개인 정보
+    UserDto user;
+
+    //공연기록
+    List<History> histories;
+
+    //받은 제안서 - 스타는 제안서 x
+    List<ProposalDto> proposalList;
+
 }
