4a2e93cefe9bdda3c445d0dac09bcef7adaf9ab5
hoin <apicad0307@gmail.com>
2022-11-17 15:34:46 +0900

    feat: reviewDto 생성

---
+++ server/src/main/java/gigsproject/gigs/response/ReviewDto.java
@@ -0,0 +1,26 @@
+package gigsproject.gigs.response;
+
+import gigsproject.gigs.domain.Host;
+import gigsproject.gigs.domain.Review;
+import gigsproject.gigs.domain.Star;
+
+import java.time.LocalDateTime;
+
+public class ReviewDto {
+
+    private Long reviewId;
+    private String content;
+    private Host host;
+    private Star star;
+    private LocalDateTime createdAt;
+    private Integer score;
+
+    public ReviewDto(Review review) {
+        this.reviewId = review.getReviewId();
+        this.content = review.getContent();
+        this.host = review.getHost();
+        this.star = review.getStar();
+        this.createdAt = review.getCreatedAt();
+        this.score = review.getScore();
+    }
+}
