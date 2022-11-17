29fa4f3a51eab15fd0fa241348f4ce4d003f3703
hoin <apicad0307@gmail.com>
2022-11-17 16:05:51 +0900

    feat: 스타 상세 api 구현

--- server/src/main/java/gigsproject/gigs/controller/StarController.java
+++ server/src/main/java/gigsproject/gigs/controller/StarController.java
@@ -1,14 +1,17 @@
 package gigsproject.gigs.controller;
 
-import gigsproject.gigs.domain.*;
 import gigsproject.gigs.request.StarSearch;
 import gigsproject.gigs.response.StarCard;
+import gigsproject.gigs.response.StarResponse;
 import gigsproject.gigs.service.StarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class StarController {
     private final StarService starService;
+
     @GetMapping("/stars")
-    public Page<StarCard> getStarCardListCond(@ModelAttribute StarSearch starSearch, Pageable pageable){
+    public Page<StarCard> getStarCardListCond(@ModelAttribute StarSearch starSearch, Pageable pageable) {
         Page<StarCard> result = starService.getStarCardListCond(starSearch, pageable);
         return result;
 
+    }
+
+    @GetMapping("/stars/{starId}")
+    public StarResponse starInfo(@PathVariable Long starId) {
+        return starService.findById(starId);
     }
 
 }
