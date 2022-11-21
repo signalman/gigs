3fa55baf78f29cb6c77070649cbc800699b5a2b0
hoin <apicad0307@gmail.com>
2022-11-21 23:20:18 +0900

    feat: 로그인시 쿠키 userName->userId로 변경

--- server/src/main/java/gigsproject/gigs/controller/UserController.java
+++ server/src/main/java/gigsproject/gigs/controller/UserController.java
@@ -23,9 +23,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.util.List;
-import java.util.Map;
 
 
 //@RestController
@@ -62,13 +60,12 @@ public class UserController {
      * 로그인 성공시 쿠키보내고 redirect 하는 컨트롤러
      */
     @GetMapping("/wait")
-    void waitLogin(HttpServletResponse response, Authentication authentication) throws IOException {
+    void waitLogin(HttpServletResponse response, @AuthenticationPrincipal OAuth2UserCustom oAuth2UserCustom) throws IOException {
 
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        Map<String, Object> oAuth2UserAttributes = (Map<String, Object>) oAuth2User.getAttributes().get("properties");
-        String userName = oAuth2UserAttributes.get("nickname").toString(); //'신호인'
+        User user = oAuth2UserCustom.getUser();
+        Long userId = user.getUserId();
 
-        Cookie cookie = new Cookie("userName", URLEncoder.encode(userName, "UTF-8"));
+        Cookie cookie = new Cookie("userId", userId.toString());
         cookie.setPath("/");
         cookie.setDomain("localhost");
         response.addCookie(cookie);
