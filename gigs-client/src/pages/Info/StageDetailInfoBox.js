7092f966b75d72f50a5df0c3c9e2f02f55c413a2
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-12-25 02:56:29 +0900

    fix: 상세정보 정보 표시 null 처리

--- gigs-client/src/pages/Info/StageDetailInfoBox.js
+++ gigs-client/src/pages/Info/StageDetailInfoBox.js
@@ -5,7 +5,7 @@ import PeopleIcon from '@mui/icons-material/People';
 import LocalAtmIcon from '@mui/icons-material/LocalAtm';
 import MicExternalOnIcon from '@mui/icons-material/MicExternalOn';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import { COLOR, ifNull } from '../../utils/Constants';
+import { COLOR, ENUM, ifNull } from '../../utils/Constants';
 import DetailInfoItem from './DetailInfoItem';
 
 const StageDetailInfoBox = ({
@@ -34,10 +34,10 @@ const StageDetailInfoBox = ({
       }}
     >
       <DetailInfoItem Icon={PhotoSizeSelectSmallIcon} onEdit={openEditAreaDialog} editable={editable} >{ifNull(stageSize, '', 'm^2')}</DetailInfoItem>
-      <DetailInfoItem Icon={PeopleIcon} onEdit={openEditTargetDialog} editable={editable}>{((targetAge || Number(targetAge) === 0) && targetGender !== 'DEFAULT' && (targetMinCount || Number(targetMinCount) === 0)) ? `${targetAge}대 ${targetGender} ${targetMinCount}인 이상` : '데이터를 추가해주세요.'}</DetailInfoItem>
+      <DetailInfoItem Icon={PeopleIcon} onEdit={openEditTargetDialog} editable={editable}>{((targetAge || Number(targetAge) === 0) && targetGender !== 'DEFAULT' && (targetMinCount || Number(targetMinCount) === 0)) ? `${targetAge}대 ${ENUM[targetGender]} ${targetMinCount}인 이상` : '데이터를 추가해주세요.'}</DetailInfoItem>
       <DetailInfoItem Icon={LocalAtmIcon} onEdit={openEditPayDialog} editable={editable}>{ifNull((pay), '', '원 이상')}</DetailInfoItem>
       <DetailInfoItem Icon={MicExternalOnIcon} editable={false}>{`${showCount}회 이상`}</DetailInfoItem>
-      <DetailInfoItem Icon={LocationOnIcon} onEdit={openEditStageTypeDialog} editable={editable}>{ifNull(stageType)}</DetailInfoItem>
+      <DetailInfoItem Icon={LocationOnIcon} onEdit={openEditStageTypeDialog} editable={editable}>{stageType ? ENUM[stageType] : '데이터를 추가해주세요.'}</DetailInfoItem>
     </Box>
   );
 };
