ffc4e6c7a83653cc8f7b387e0a5c768b0c8a6082
win_min <60373714+psm9718@users.noreply.github.com>
2022-12-26 20:00:28 +0900

    fix: 리뷰 작성자 이름, 대표이미지 필드 추가

--- server/src/main/java/gigsproject/gigs/response/ReviewDto.java
+++ server/src/main/java/gigsproject/gigs/response/ReviewDto.java
@@ -4,21 +4,35 @@ import gigsproject.gigs.domain.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 //상대방으로부터 어떤 포스트인지는 상관없이 그냥 이 무대에대한 리뷰
 public class ReviewDto {
 
     private Long reviewId;
+
+    /**
+     * 작성자 정보
+     */
     private Long fromRoleId; //리뷰작성자의 호스트 or 스타 id
     private String fromRole;
+    private String name;
+    private String repImg;
 
+    /**
+     * 대상자 정보
+     */
     private Long toRoleId; //리뷰 대상자의 호스트 or 스타 id
     private String toRole;
 
+    /**
+     * 리뷰 정보
+     */
     private String content; // 작성자가 쓴 내용
     private Double score;
     private LocalDateTime createdAt;
+
 
 
     public ReviewDto(Review review) {
@@ -27,14 +41,20 @@ public class ReviewDto {
         Star star = review.getProposal().getStar();
 
         //작성자, 작성 대상 판단
-        if (host.getUser().getUserId() == review.getUser().getUserId()) {
+        if (Objects.equals(host.getUser().getUserId(), review.getUser().getUserId())) {
             fromRoleId = host.getHostId();
             fromRole = Role.ROLE_HOST.toString().toLowerCase();
+            name = host.getStageName();
+            repImg = host.getRepImg();
+
             toRoleId = star.getStarId();
             toRole = Role.ROLE_STAR.toString().toLowerCase();
         } else {
             fromRoleId = star.getStarId();
             fromRole = Role.ROLE_STAR.toString().toLowerCase();
+            name = star.getName();
+            repImg = star.getRepImg();
+
             toRoleId = host.getHostId();
             toRole = Role.ROLE_HOST.toString().toLowerCase();
         }
